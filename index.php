b772de89c450bb8a0ac2d2daa14c468264b75eb2
Mayenziwe <mayenziwemaseko06@gmail.com>
2024-11-15 18:33:18 +0200

    Linked to bootstrap icons CDN, added them to location and venue text

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="public/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
 </head>
 
@@ -40,10 +41,10 @@
                                 <!-- <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate in consequuntur, tenetur nulla autem excepturi eius?</p> -->
                                 <div class="details d-flex align-items-end justify-content-between">
                                     <div class="d-flex flex-column">
-                                        <span class="date" style="font-size: small;">Sat, 30 Nov 2024, 16:00</span>
-                                        <span class="location" style="font-size: small;">Somhlolo National Stadium</span>
+                                        <div><i class="bi bi-calendar-event me-1"></i><span class="date" style="font-size: small;">Sat, 30 Nov 2024, 16:00</span></div>
+                                        <div><i class="bi bi-geo me-1"></i><span class="location" style="font-size: small;">Somhlolo National Stadium</span></div>
                                     </div>
-                                    <span class="price fw-bold fs-5" style="color: orange" >E500.00</span>
+                                    <span class="price fw-bold fs-5" style="color: orange">E500.00</span>
                                 </div>
                             </div>
                         </div>
@@ -97,7 +98,7 @@
     </main>
 
     <?php
-        include "views/footer.php";
+    include "views/footer.php";
     ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
