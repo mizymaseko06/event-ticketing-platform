999c43126c7cebc377b2b117bd6c0cf51653b191
Mayenziwe <mayenziwemaseko06@gmail.com>
2024-11-15 17:36:31 +0200

    Separated header and footer into separate files from index

--- index.php
+++ index.php
@@ -11,38 +11,7 @@
 </head>
 
 <body>
-    <header>
-        <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-            <div class="container">
-                <a class="navbar-brand" href="#">
-                    <img src="public/images/ticket-logo_9850-381.png" class="img-fluid" style="max-height: 40px" alt="">
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Events</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">How It Works</a>
-                        </li>
-
-                        </li>
-                    </ul>
-
-                    <div class="d-flex align-content-center">
-                        <p class="nav-item white-text">My tickets</p>
-                        <p class="nav-item white-text">E200.00</p>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </header>
+    <?php include "views/header.php"; ?>
     <main>
         <section id="hero">
             <div class="container" style="padding-top: 200px;">
@@ -126,41 +95,10 @@
             </div>
         </section>
     </main>
-    <footer>
-        <div class="container-fluid" style="background-color: black;">
-            <p class="lead text-center white-text pt-4">Ready to make memories? Start exploring now!</p>
-            <div class="row d-flex flex-row justify-content-center">
-                <div class="col-lg-5 col-sm-10 d-flex flex-column align-items-lg-end align-items-sm-center text-center text-md-end my-3">
-                    <div>
 
-                        <h5 style="color: orange;">Site Map</h5>
-                        <div id="site-map" class="d-flex flex-column">
-                            <span><a href="">Events</a></span>
-                            <span><a href="">About Us</a></span>
-                            <span><a href="">FAQ</a></span>
-                            <span><a href="">Privacy Policy</a></span>
-                            <span><a href="">Terms of service</a></span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 col-sm-10 d-flex flex-column align-items-lg-start align-items-sm-center text-center text-md-start m-3">
-                    
-                    <h5 style="color: orange;">Contact</h5>
-                    <div class="d-flex flex-column">
-                        <span class="white-text">Phone: 2404 0000</span>
-                        <span class="white-text">Email: ask@events.co.sz</span>
-                        <div class="icons">
-                            <img class="social-links" src="public/images/Facebook.png" alt="">
-                            <img class="social-links" src="public/images/Instagram.png" alt="">
-                            <img class="social-links" src="public/images/TwitterX.png" alt="">
-                            <img class="social-links" src="public/images/LinkedIn Circled.png" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-
+    <?php
+        include "views/footer.php";
+    ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
---
+++ views/footer.php
@@ -0,0 +1,34 @@
+<footer>
+        <div class="container-fluid" style="background-color: black;">
+            <p class="lead text-center white-text pt-4">Ready to make memories? Start exploring now!</p>
+            <div class="row d-flex flex-row justify-content-center">
+                <div class="col-lg-5 col-sm-10 d-flex flex-column align-items-lg-end align-items-sm-center text-center text-md-end my-3">
+                    <div>
+
+                        <h5 style="color: orange;">Site Map</h5>
+                        <div id="site-map" class="d-flex flex-column">
+                            <span><a href="">Events</a></span>
+                            <span><a href="">About Us</a></span>
+                            <span><a href="">FAQ</a></span>
+                            <span><a href="">Privacy Policy</a></span>
+                            <span><a href="">Terms of service</a></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-sm-10 d-flex flex-column align-items-lg-start align-items-sm-center text-center text-md-start m-3">
+                    
+                    <h5 style="color: orange;">Contact</h5>
+                    <div class="d-flex flex-column">
+                        <span class="white-text">Phone: 2404 0000</span>
+                        <span class="white-text">Email: ask@events.co.sz</span>
+                        <div class="icons">
+                            <img class="social-links" src="public/images/Facebook.png" alt="">
+                            <img class="social-links" src="public/images/Instagram.png" alt="">
+                            <img class="social-links" src="public/images/TwitterX.png" alt="">
+                            <img class="social-links" src="public/images/LinkedIn Circled.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
