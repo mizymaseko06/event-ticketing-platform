c7efb9264a82b9e4d1d3dda31c98d024d0651d06
Mayenziwe <mayenziwemaseko06@gmail.com>
2024-11-15 17:25:28 +0200

    Created footer, added and aligned footer content

--- index.php
+++ index.php
@@ -95,38 +95,71 @@
 
                 <div class="row d-flex justify-content-center">
 
-                        <div class="d-flex flex-row align-items-center col-7 mb-3">
-                            <div class="image w-25">
-                                <img src="public/images/rb_3598.png" class="img-fluid" alt="">
-                            </div>
-                            <div>
-                                <h3>Find events</h3>
-                                <p>Browse a variety of events tailored to your interests and location.</p>
-                            </div>
+                    <div class="d-flex flex-row align-items-center col-7 mb-3">
+                        <div class="image w-25">
+                            <img src="public/images/rb_3598.png" class="img-fluid" alt="">
                         </div>
-                        <div class="d-flex flex-row align-items-center col-7 mb-5">
-                            <div class="image w-25">
-                                <img src="public/images/rb_96.png" class="img-fluid" alt="">
-                            </div>
-                            <div>
-                                <h3>Purchase tickets</h3>
-                                <p>Reserve your spot by purchasing tickets with just a few clicks.</p>
-                            </div>
+                        <div>
+                            <h3>Find events</h3>
+                            <p>Browse a variety of events tailored to your interests and location.</p>
                         </div>
-                        <div class="d-flex flex-row align-items-center col-7 mb-5">
-                            <div class="image w-25">
-                                <img src="public/images/rb_1749.png" class="img-fluid" alt="">
-                            </div>
-                            <div>
-                                <h3>Get ready to go!</h3>
-                                <p>Receive confirmation and all the info you need to make most of your event.</p>
-                            </div>
+                    </div>
+                    <div class="d-flex flex-row align-items-center col-7 mb-5">
+                        <div class="image w-25">
+                            <img src="public/images/rb_96.png" class="img-fluid" alt="">
                         </div>
+                        <div>
+                            <h3>Purchase tickets</h3>
+                            <p>Reserve your spot by purchasing tickets with just a few clicks.</p>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-row align-items-center col-7 mb-5">
+                        <div class="image w-25">
+                            <img src="public/images/rb_1749.png" class="img-fluid" alt="">
+                        </div>
+                        <div>
+                            <h3>Get ready to go!</h3>
+                            <p>Receive confirmation and all the info you need to make most of your event.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
     </main>
-    <footer></footer>
+    <footer>
+        <div class="container-fluid" style="background-color: black;">
+            <p class="lead text-center white-text pt-4">Ready to make memories? Start exploring now!</p>
+            <div class="row d-flex flex-row justify-content-center">
+                <div class="col-lg-5 col-sm-10 d-flex flex-column align-items-lg-end align-items-sm-center text-center text-md-end my-3">
+                    <div>
+
+                        <h5 style="color: orange;">Site Map</h5>
+                        <div id="site-map" class="d-flex flex-column">
+                            <span><a href="">Events</a></span>
+                            <span><a href="">About Us</a></span>
+                            <span><a href="">FAQ</a></span>
+                            <span><a href="">Privacy Policy</a></span>
+                            <span><a href="">Terms of service</a></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-sm-10 d-flex flex-column align-items-lg-start align-items-sm-center text-center text-md-start m-3">
+                    
+                    <h5 style="color: orange;">Contact</h5>
+                    <div class="d-flex flex-column">
+                        <span class="white-text">Phone: 2404 0000</span>
+                        <span class="white-text">Email: ask@events.co.sz</span>
+                        <div class="icons">
+                            <img class="social-links" src="public/images/Facebook.png" alt="">
+                            <img class="social-links" src="public/images/Instagram.png" alt="">
+                            <img class="social-links" src="public/images/TwitterX.png" alt="">
+                            <img class="social-links" src="public/images/LinkedIn Circled.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
