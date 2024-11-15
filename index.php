c458d05f9a45e7dd9cc7f2ef2528c5190522090b
Mayenziwe <mayenziwemaseko06@gmail.com>
2024-11-15 09:53:24 +0200

    Created structure of navigation bar

--- index.php
+++ index.php
@@ -10,7 +10,38 @@
 </head>
 
 <body>
-    <header></header>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="public/images/ticket-logo_9850-381.png" class="img-fluid" style="max-height: 40px" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Events</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">How It Works</a>
+                        </li>
+
+                        </li>
+                    </ul>
+
+                    <div class="d-flex align-content-center">
+                        <p class="nav-item">My tickets</p>
+                        <p class="nav-item">E200.00</p>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
     <main></main>
     <footer></footer>
 
