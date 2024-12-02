80248b90612ea4ebebbc81500141066a32fb3bb8
Mayenziwe Maseko <86263980+mizymaseko06@users.noreply.github.com>
2024-12-02 08:50:58 +0200

    Configured event page to work with actual event, similar events also works

--- event.php
+++ event.php
@@ -1,3 +1,38 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['userID'])) {
+    // Redirect to login page if not logged in
+    header("Location: login.php");
+    exit();
+}
+
+include "db/db_conn.php";
+
+// Check if event ID is passed in the URL
+if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $eventID = $_GET['id'];
+
+    // Prepare SQL to fetch the event details
+    $stmt = $conn->prepare("SELECT eventName, description, date, time, location, price, image FROM events WHERE eventID = ?");
+    $stmt->bind_param("i", $eventID);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    // Check if the event exists
+    if ($result->num_rows > 0) {
+        $event = $result->fetch_assoc();
+    } else {
+        // Redirect to a 404 or error page if event not found
+        header("Location: 404.php");
+        exit();
+    }
+} else {
+    // Redirect to a 404 or error page if no valid ID is provided
+    header("Location: 404.php");
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,23 +50,23 @@
     <?php include "header.php"; ?>
     <main>
         <div class="container-fluid p-0" style="margin-top: 60px;">
-            <img src="public/images/hero-background.jpg" class="img-fluid w-100" style="object-fit: cover; height: 50vh;" alt="">
+            <img src="<?php echo htmlspecialchars($event['image']); ?>" class="img-fluid w-100" style="object-fit: cover; height: 50vh;" alt="">
         </div>
         <div class="container-sm detailed-event">
             <div class="row my-3">
-                <h5 class="fw-bolder">Event</h5>
+                <h5 class="fw-bolder"><?php echo htmlspecialchars($event['eventName']); ?></h5>
                 <div class="d-flex flex-column justify-content-between mt-3">
                     <div class="details d-flex align-items-end justify-content-between">
                         <div class="d-flex flex-column">
-                            <div><i class="bi bi-calendar-event me-1"></i><span class="date" style="font-size: small;">Sat, 30 Nov 2024, 16:00</span></div>
-                            <div><i class="bi bi-geo me-1"></i><span class="location" style="font-size: small;">Somhlolo National Stadium</span></div>
+                            <div><i class="bi bi-calendar-event me-1"></i><span class="date" style="font-size: small;"><?php echo date("D, d M Y, H:i", strtotime($event['date'] . ' ' . $event['time'])); ?></span></div>
+                            <div><i class="bi bi-geo me-1"></i><span class="location" style="font-size: small;"><?php echo htmlspecialchars($event['location']); ?></span></div>
                         </div>
-                        <span class="price fw-bold fs-5" style="color: orange">E500.00</span>
+                        <span class="price fw-bold fs-5" style="color: orange">E<?php echo number_format($event['price'], 2); ?></span>
                     </div>
                 </div>
                 <div class="description mt-3">
                     <h5>Desciption</h5>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas quod aliquam magni iste adipisci consequuntur laudantium! Architecto, harum doloribus est id impedit ut at veniam culpa, enim voluptatem amet iste illum consequatur.</p>
+                    <p><?php echo nl2br(htmlspecialchars($event['description'])); ?></p>
                 </div>
 
 
@@ -53,27 +88,35 @@
                     Similar Events
                 </h2>
                 <div class="row justify-content-center">
-                    <?php
-                    for ($i = 0; $i < 6; $i++) {
-                    ?>
-                        <div class="card col-10 col-md-5 col-lg-3 m-1">
-                            <p class="card-header text-end" style="background-color: white;">Category</p>
-                            <img src="public/images/hero-background.jpg" class="card-img-top">
-                            <div class="card-body index-card-body d-flex flex-column justify-content-between">
-                                <h5 class="card-title fw-bolder">Event</h5>
-                                <!-- <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate in consequuntur, tenetur nulla autem excepturi eius?</p> -->
-                                <div class="details d-flex align-items-end justify-content-between">
-                                    <div class="d-flex flex-column">
-                                        <div><i class="bi bi-calendar-event me-1"></i><span class="date" style="font-size: small;">Sat, 30 Nov 2024, 16:00</span></div>
-                                        <div><i class="bi bi-geo me-1"></i><span class="location" style="font-size: small;">Somhlolo National Stadium</span></div>
+                    <div class="row justify-content-center">
+                        <?php
+                        $similarSql = "SELECT eventID, eventName, date, time, location, price, image FROM events WHERE eventID != ? ORDER BY RAND() LIMIT 6";
+                        $similarStmt = $conn->prepare($similarSql);
+                        $similarStmt->bind_param("i", $eventID);
+                        $similarStmt->execute();
+                        $similarResult = $similarStmt->get_result();
+
+                        while ($similar = $similarResult->fetch_assoc()) {
+                        ?>
+                            <div class="card col-10 col-md-5 col-lg-3 m-1">
+                                <a href="event.php?id=<?php echo $similar['eventID']; ?>" class="event-link">
+                                    <img src="<?php echo htmlspecialchars($similar['image']); ?>" class="card-img-top">
+                                    <div class="card-body index-card-body d-flex flex-column justify-content-between">
+                                        <h5 class="card-title fw-bolder"><?php echo htmlspecialchars($similar['eventName']); ?></h5>
+                                        <div class="details d-flex align-items-end justify-content-between">
+                                            <div class="d-flex flex-column">
+                                                <div><i class="bi bi-calendar-event me-1"></i><span class="date" style="font-size: small;"><?php echo date("D, d M Y, H:i", strtotime($similar['date'] . ' ' . $similar['time'])); ?></span></div>
+                                                <div><i class="bi bi-geo me-1"></i><span class="location" style="font-size: small;"><?php echo htmlspecialchars($similar['location']); ?></span></div>
+                                            </div>
+                                            <span class="price fw-bold fs-5" style="color: orange">E<?php echo number_format($similar['price'], 2); ?></span>
+                                        </div>
                                     </div>
-                                    <span class="price fw-bold fs-5" style="color: orange">E500.00</span>
-                                </div>
+                                </a>
                             </div>
-                        </div>
-                    <?php
-                    }
-                    ?>
+                        <?php
+                        }
+                        ?>
+                    </div>
                 </div>
 
             </div>
